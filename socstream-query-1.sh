1b57c8955cb6ca5eb0db05367cb4e52b8ccb33cc
Giacomo Marciani <giacomo.marciani@gmail.com>
2017-06-28 17:33:02 +0200

    conflicts solved

--- socstream-query-1.sh
+++ socstream-query-1.sh
@@ -20,8 +20,8 @@ SOCSTREAM_OPTS="${SOCSTREAM_OPTS} --kafka.zookeeper localhost:2181"
 SOCSTREAM_OPTS="${SOCSTREAM_OPTS} --kafka.bootstrap localhost:9092"
 SOCSTREAM_OPTS="${SOCSTREAM_OPTS} --kafka.topic socstream-topic-query-1"
 SOCSTREAM_OPTS="${SOCSTREAM_OPTS} --parallelism 1"
-SOCSTREAM_OPTS="${SOCSTREAM_OPTS} --data ${SOCSTREAM_HOME}/data/test/dataset-test.txt"
-SOCSTREAM_OPTS="${SOCSTREAM_OPTS} --metadata ${SOCSTREAM_HOME}/data/test/dataset-test.txt"
+SOCSTREAM_OPTS="${SOCSTREAM_OPTS} --data ${SOCSTREAM_HOME}/data/test/dataset.txt"
+SOCSTREAM_OPTS="${SOCSTREAM_OPTS} --metadata ${SOCSTREAM_HOME}/data/test/metadata.yml"
 SOCSTREAM_OPTS="${SOCSTREAM_OPTS} --parallelism 1"
 
 ##
