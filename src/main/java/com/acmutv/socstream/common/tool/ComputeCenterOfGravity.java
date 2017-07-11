f62a1027268380cfc8f97f2f7102e4ceefac1d97
Michele Porretta <micheleporretta@gmail.com>
2017-07-11 10:44:47 +0200

    ComputeCenterOfGravity

---
+++ src/main/java/com/acmutv/socstream/common/tool/ComputeCenterOfGravity.java
@@ -0,0 +1,75 @@
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2017 Giacomo Marciani
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+ */
+package com.acmutv.socstream.common.tool;
+
+import com.acmutv.socstream.common.tuple.Coordinate;
+import com.acmutv.socstream.common.tuple.GridCoordinate;
+import com.acmutv.socstream.common.tuple.RichSensorEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * This class gives the center of gravity between 2 points with their coordinates.
+ * The origin of the axes coincides with the center of the game field
+ * @author Giacomo Marciani {@literal <gmarciani@acm.org>}
+ * @author Michele Porretta {@literal <mporretta@acm.org>}
+ * @since 1.0
+ */
+public class ComputeCenterOfGravity {
+
+  /**
+   * Compute Center of Gravity between a point (x,y) and a Coordinate
+   * @param currentX
+   * @param currentY
+   * @param last
+   * @return
+   */
+  public static Coordinate compute(long currentX, long currentY, Coordinate last) {
+
+    long x = (currentX + last.getX())/2;
+    long y = (currentX + last.getY())/2;
+
+    return new Coordinate(x,y);
+  }
+
+  /**
+   * Compute Center of Gravity between a point (x,y) and a GridCoordinate
+   * @param currentX
+   * @param currentY
+   * @param last
+   * @return
+   */
+  public static Coordinate computeWithCell(long currentX, long currentY, GridCoordinate last) {
+
+    long x = (currentX + last.getX())/2;
+    long y = (currentX + last.getY())/2;
+
+    return new Coordinate(x,y);
+  }
+}
